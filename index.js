28994eb9f55d625537650a28dac62a33196a48a9
MNDSE <mndsezi@gmail.com>
2022-03-23 19:08:05 +0000

    commiting for the first time

---
+++ index.js
@@ -0,0 +1,28 @@
+
+// This gives the ability to users to enter there won groupe
+
+const element = document.getElementById("myH1");
+
+element.textContent = window.prompt("PLEASE GIVE A TITLE TO WHAT YOU ARE COUNTING ! like (PEOPLE)");
+document.body.append(element);
+
+
+// Creating the wallent
+let myTotal = document.getElementById("myTotal");
+let meDepot = document.getElementById("myH2");
+
+let count = 0;
+
+function ajout(){
+   
+    count = count + 1;
+    
+    meDepot.innerText = count;
+
+}
+
+function enregistrer(){
+    let newCount = count + "-";
+    myTotal.innerText += newCount;
+    console.log(count)
+}
